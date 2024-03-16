acc2d3179f74c08c5da7f21231e6da6af460fa52
LcCompany <96777677+LcCompany@users.noreply.github.com>
2024-03-16 21:04:08 +0000

    3

--- Hello.py
+++ Hello.py
@@ -2,16 +2,23 @@ import streamlit as st
 import requests
 
 def zoek_uitspraken(onderwerp, zaaknummer, ecli):
-    # Base URL voor het opvragen van uitspraakdocumenten
     base_url = "http://data.rechtspraak.nl/uitspraken/zoeken"
     params = {}
     
-    if ecli:
-        params['id'] = ecli  # Zoeken op ECLI
     if onderwerp:
-        params['subject'] = onderwerp  # Zoeken op rechtsgebied, als benadering van 'onderwerp'
+        # Aangezien 'onderwerp' niet direct als parameter gebruikt kan worden,
+        # zouden we andere gerelateerde parameters moeten overwegen.
+        # Deze placeholder moet mogelijk aangepast worden naar een passende implementatie.
+        # Voor deze demo, simuleren we een generieke zoekparameter.
+        params['q'] = onderwerp  # Dit is een fictieve parameter voor demonstratiedoeleinden
+    
     if zaaknummer:
-        params['psi:zaaknummer'] = zaaknummer  # Direct zoeken op zaaknummer indien beschikbaar
+        # Zoeken op zaaknummer, indien deze parameter beschikbaar en correct is
+        params['zaaknummer'] = zaaknummer
+
+    if ecli:
+        # Direct zoeken op ECLI
+        params['ecli'] = ecli
 
     response = requests.get(base_url, params=params)
     if response.status_code == 200:
@@ -22,9 +29,8 @@ def zoek_uitspraken(onderwerp, zaaknummer, ecli):
     else:
         return "Geen resultaten gevonden of een fout opgetreden."
 
-# Streamlit UI
+# Streamlit UI componenten
 st.title('Zoeken naar rechterlijke uitspraken')
-
 onderwerp = st.text_input("Onderwerp")
 zaaknummer = st.text_input("Zaaknummer")
 ecli = st.text_input("ECLI")
