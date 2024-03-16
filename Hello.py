99abd6e0177ae3b7bad56ba3918216f18b4f79d4
LcCompany <96777677+LcCompany@users.noreply.github.com>
2024-03-16 20:54:18 +0000

    v1

--- Hello.py
+++ Hello.py
@@ -1,51 +1,34 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
+import requests
 
-LOGGER = get_logger(__name__)
+# Functie om de Rechtspraak API te bevragen
+def zoek_uitspraken(onderwerp, zaaknummer, ecli):
+    base_url = "http://data.rechtspraak.nl/uitspraken/content"
+    params = {
+        'id': ecli,  # Gebruik ECLI als belangrijkste zoekparameter
+        # Voeg eventueel meer parameters toe zoals onderwerp en zaaknummer
+        # afhankelijk van de beschikbaarheid en de benodigdheden van de API.
+    }
+    response = requests.get(base_url, params=params)
+    if response.status_code == 200:
+        return response.json()  # Of verwerk de XML-antwoord indien de API dat formaat gebruikt
+    else:
+        return "Geen resultaten gevonden of een fout opgetreden."
 
+# Streamlit applicatie layout
+st.title('Zoeken naar rechterlijke uitspraken')
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+# Input velden voor de gebruiker
+onderwerp = st.text_input("Onderwerp (nog niet geïmplementeerd)")
+zaaknummer = st.text_input("Zaaknummer (nog niet geïmplementeerd)")
+ecli = st.text_input("ECLI")
 
-    st.write("# Welcome to Streamlit! 👋")
+# Zoekactie
+if st.button('Zoek'):
+    with st.spinner('Zoeken...'):
+        resultaten = zoek_uitspraken(onderwerp, zaaknummer, ecli)
+        # Dit toont de ruwe JSON-respons. Pas de presentatie aan naar wens.
+        st.write(resultaten)
 
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
-
-
-if __name__ == "__main__":
-    run()
+# Let op: dit voorbeeld gebruikt de ECLI-parameter als primaire zoekterm.
+# U moet de API-documentatie raadplegen voor de exacte parameterstructuur en de responsverwerking aanpassen.
