5f539f4b8bd850a71207436e0840176c8f3d3c30
LcCompany <96777677+LcCompany@users.noreply.github.com>
2024-03-16 21:07:56 +0000

    4

--- Hello.py
+++ Hello.py
@@ -1,24 +1,21 @@
 import streamlit as st
 import requests
 
-def zoek_uitspraken(onderwerp, zaaknummer, ecli):
+def zoek_uitspraken(onderwerp, zaaknummer, ecli, sorteer):
     base_url = "http://data.rechtspraak.nl/uitspraken/zoeken"
     params = {}
     
     if onderwerp:
-        # Aangezien 'onderwerp' niet direct als parameter gebruikt kan worden,
-        # zouden we andere gerelateerde parameters moeten overwegen.
-        # Deze placeholder moet mogelijk aangepast worden naar een passende implementatie.
-        # Voor deze demo, simuleren we een generieke zoekparameter.
-        params['q'] = onderwerp  # Dit is een fictieve parameter voor demonstratiedoeleinden
+        params['q'] = onderwerp
     
     if zaaknummer:
-        # Zoeken op zaaknummer, indien deze parameter beschikbaar en correct is
         params['zaaknummer'] = zaaknummer
 
     if ecli:
-        # Direct zoeken op ECLI
         params['ecli'] = ecli
+
+    if sorteer:
+        params['sort'] = 'date desc'  # Aannemende dat 'date desc' de API zal instrueren om aflopend te sorteren op datum
 
     response = requests.get(base_url, params=params)
     if response.status_code == 200:
@@ -34,8 +31,9 @@ st.title('Zoeken naar rechterlijke uitspraken')
 onderwerp = st.text_input("Onderwerp")
 zaaknummer = st.text_input("Zaaknummer")
 ecli = st.text_input("ECLI")
+sorteer = st.checkbox("Sorteer op nieuwste uitspraken eerst", value=True)
 
 if st.button('Zoek'):
     with st.spinner('Zoeken...'):
-        resultaten = zoek_uitspraken(onderwerp, zaaknummer, ecli)
+        resultaten = zoek_uitspraken(onderwerp, zaaknummer, ecli, sorteer)
         st.write(resultaten)  # Aanpassing nodig voor nette presentatie van resultaten
