461f8f16fd5a73ff3449a31b55d351a49e1b3b50
LcCompany <96777677+LcCompany@users.noreply.github.com>
2024-03-16 20:58:49 +0000

    v2

--- Hello.py
+++ Hello.py
@@ -1,34 +1,35 @@
 import streamlit as st
 import requests
 
-# Functie om de Rechtspraak API te bevragen
 def zoek_uitspraken(onderwerp, zaaknummer, ecli):
-    base_url = "http://data.rechtspraak.nl/uitspraken/content"
-    params = {
-        'id': ecli,  # Gebruik ECLI als belangrijkste zoekparameter
-        # Voeg eventueel meer parameters toe zoals onderwerp en zaaknummer
-        # afhankelijk van de beschikbaarheid en de benodigdheden van de API.
-    }
+    # Base URL voor het opvragen van uitspraakdocumenten
+    base_url = "http://data.rechtspraak.nl/uitspraken/zoeken"
+    params = {}
+    
+    if ecli:
+        params['id'] = ecli  # Zoeken op ECLI
+    if onderwerp:
+        params['subject'] = onderwerp  # Zoeken op rechtsgebied, als benadering van 'onderwerp'
+    if zaaknummer:
+        params['psi:zaaknummer'] = zaaknummer  # Direct zoeken op zaaknummer indien beschikbaar
+
     response = requests.get(base_url, params=params)
     if response.status_code == 200:
-        return response.json()  # Of verwerk de XML-antwoord indien de API dat formaat gebruikt
+        try:
+            return response.json()
+        except ValueError:
+            return response.text
     else:
         return "Geen resultaten gevonden of een fout opgetreden."
 
-# Streamlit applicatie layout
+# Streamlit UI
 st.title('Zoeken naar rechterlijke uitspraken')
 
-# Input velden voor de gebruiker
-onderwerp = st.text_input("Onderwerp (nog niet geïmplementeerd)")
-zaaknummer = st.text_input("Zaaknummer (nog niet geïmplementeerd)")
+onderwerp = st.text_input("Onderwerp")
+zaaknummer = st.text_input("Zaaknummer")
 ecli = st.text_input("ECLI")
 
-# Zoekactie
 if st.button('Zoek'):
     with st.spinner('Zoeken...'):
         resultaten = zoek_uitspraken(onderwerp, zaaknummer, ecli)
-        # Dit toont de ruwe JSON-respons. Pas de presentatie aan naar wens.
-        st.write(resultaten)
-
-# Let op: dit voorbeeld gebruikt de ECLI-parameter als primaire zoekterm.
-# U moet de API-documentatie raadplegen voor de exacte parameterstructuur en de responsverwerking aanpassen.
+        st.write(resultaten)  # Aanpassing nodig voor nette presentatie van resultaten
